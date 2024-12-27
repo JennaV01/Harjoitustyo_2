dac48c8ee1f93b1b965ebd822cc3d2d0a7796374
JennaV01 <jljvirolainen@gmail.com>
2024-12-27 21:26:24 +0200

    Project Files

---
+++ Source/JV_UE/JV_UE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "JV_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JV_UE, "JV_UE" );
